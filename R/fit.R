bc80e880c126da849a6d90f09b19f7f0d1d8ca92
alexpavlakis <alexpavlakis@gmail.com>
2020-03-17 18:29:11 -0700

    readme

--- R/fit.R
+++ R/fit.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(rstan)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
@@ -23,7 +24,7 @@ polls <- bind_rows(polls_data) %>%
   arrange(state)
 
 # Vector of state names (51 = all states + DC)
-state <- gsub("_", " ", unique(polls$state))
+state <- prior_results$state
 
 
 
@@ -94,14 +95,14 @@ results_biden <- data_frame(
   cand  = 'biden')
 
 results_trump <- data_frame(
-    state = state,
-    lower = quantiles_trump[1, ],
-    mean  = means_trump,
-    upper = quantiles_trump[2, ],
-    cand  = 'trump')
+  state = state,
+  lower = quantiles_trump[1, ],
+  mean  = means_trump,
+  upper = quantiles_trump[2, ],
+  cand  = 'trump')
 
 # Plot
-p <- results_biden %>%
+state_plot <- results_biden %>%
   ggplot() +
   aes(x = mean, y = reorder(state, mean),
       xmin = lower, xmax = upper) +
@@ -122,20 +123,56 @@ p <- results_biden %>%
        title = "estimated vote share by state",
        subtitle = paste0("source: state polls via RCP as of ", Sys.Date())) +
   theme(plot.title = element_text(size = 15, face = "bold"),
-        plot.subtitle = element_text(size = 7))
+        plot.subtitle = element_text(size = 7),
+        axis.text.y = element_text(size = 11))
 
-p
+ggsave("results/state_distributions.png", state_plot)
 
 # P-win --------------------------------------------------------------------
 
-# Probability f winning the state
-res <- apply(em$theta, 2, function(x) mean(x[, 2] > x[, 1]))
-names(res) <- state
-round(res, 3)
+# Probability of winning the state
+p_biden <- round(apply(em$theta, 2, function(x) mean(x[, 2] > x[, 1])), 3)
+names(p_biden) <- state
+
+write.csv(p_biden, "results/p_biden.csv", row.names = F)
 
 
 
+# Simulate
+er <- matrix(0, nrow = dim(em$theta)[1], ncol = dim(em$theta)[3])
+
+# Simulate some elections and their results
+for(i in 1:dim(em$theta)[1]) {
+  for(s in 1:dim(em$theta)[2]) {
+    for(o in 1:dim(em$theta)[3]) {
+      if(em$theta[i, s, o] == max(em$theta[i, s, ])) {
+        er[i, o] <- er[i, o] + prior_results$ev[s]
+      }
+    }
+  }
+}
 
 
+ec_plot <- data_frame(
+  electoral_votes = c(er[, 1], er[, 2]),
+  candidate = c(rep("Trump", nrow(er)), rep("Biden", nrow(er)))
+) %>% 
+  ggplot() +
+  aes(x = electoral_votes, fill = candidate, y = ..density..) +
+  geom_histogram(alpha = 0.75, bins = 60,
+                 position = "identity") +
+  scale_x_continuous(limits = c(0, 538),
+                     breaks = c(seq(0, 538, 100), 270)) +
+  scale_fill_manual(values = c("blue", "red")) +
+  theme_minimal() +
+  labs(x = "electoral college votes", y = NULL, fill = NULL,
+       title = "distribution of electoral college votes") +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 15, face = "bold"),
+        plot.subtitle = element_text(size = 8),
+        panel.grid = element_blank(),
+        axis.text.y = element_blank(),
+        axis.text.x = element_text(size = 11))
 
+ggsave("results/ec_distributions.png", ec_plot)
 
